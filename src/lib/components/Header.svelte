ec3eacfceec65796cdee5f0cfb26a24ebd19b7e7
Dylan Dupasquier <dylandupasquier@gmail.com>
2024-02-25 13:25:18 -0800

    retry

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -13,7 +13,7 @@
 		<!-- <svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
 		</svg> -->
-		<ul>
+		<!-- <ul>
 			<li aria-current={$page.url.pathname === "/" ? "page" : undefined}>
 				<a href="/">RSVP</a>
 			</li>
@@ -31,7 +31,7 @@
 			>
 				<a href="/pics">Pics</a>
 			</li>
-		</ul>
+		</ul> -->
 		<!-- <svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
 		</svg> -->
